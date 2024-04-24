fb1ed2a9f6568391ddff88697a5b1931c6468ed8
xTrayambak <xtrayambak@gmail.com>
2024-04-24 06:35:54 +0530

    (xxx) i dont know

--- src/ferusgfx.nim
+++ src/ferusgfx.nim
@@ -1,4 +1,4 @@
 import
-  ferusgfx/[drawable, scene, fontmgr, displaylist, textnode, camera, imagenode], vmath
+  ferusgfx/[drawable, scene, fontmgr, displaylist, textnode, camera, imagenode, gifnode], vmath
 
-export fontmgr, drawable, scene, displaylist, textnode, vmath, imagenode, camera
+export fontmgr, drawable, scene, displaylist, textnode, vmath, imagenode, camera, gifnode
